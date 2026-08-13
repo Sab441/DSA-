e277c75904b69c86ee1977904b1878a1d76fc267
Saba Parveen <saba24276@gmail.com>
2026-08-14 00:08:20 +0530

    Create 88-Merge-Sorted-Array.cpp

---
+++ Two-Pointers/88-Merge-Sorted-Array.cpp
@@ -0,0 +1,40 @@
+//Leetcode 88
+//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+//Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+//The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> res(m+n);
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                res[k]=nums1[i];
+                i++;
+                k++;
+            }
+            else{
+                res[k]=nums2[j];
+                j++;
+                k++;
+            }
+        }
+            while(j<n){
+                res[k]=nums2[j];
+                j++;
+                k++;
+            }
+            while(i<m){
+                res[k]=nums1[i];
+                i++;
+                k++;
+        }
+        for(int i=0;i<m+n;i++){
+            nums1[i]=res[i];
+        }
+    }
+};
